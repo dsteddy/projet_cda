4066c0388e293a5ff65b9d9ab4100c7dfd15bb8f
DA_SILVA Teddy <te2dydasilva@gmail.com>
2024-08-01 16:20:45 +0200

    added remove old offers

--- js_scripts/scrap.js
+++ js_scripts/scrap.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
-async function scrap(jobTitle, maxPage) {
-    const browser = await puppeteer.launch();
+async function scrap(jobTitle = "data", maxPage = 40) {
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
     const customUa = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36';
@@ -11,53 +11,66 @@ async function scrap(jobTitle, maxPage) {
     const results = [];
     let offersFound = false;
 
-    for (let pageNumber = 1; pageNumber <= maxPage; pageNumber++) {
-        let url = `https://www.welcometothejungle.com/fr/jobs?refinementList%5Boffices.country_code%5D%5B%5D=FR&query=${jobTitle}&page=${pageNumber}`;
+    try {
+        for (let pageNumber = 1; pageNumber <= maxPage; pageNumber++) {
+            // console.log('Page:', pageNumber)
+            let url = `https://www.welcometothejungle.com/fr/jobs?refinementList%5Boffices.country_code%5D%5B%5D=FR&query=${jobTitle}&page=${pageNumber}`;
 
-        try {
-            await page.goto(url, { waitUntil: "networkidle0" });
-            await page.setViewport({ width: 1080, height: 1024 });
+            try {
+                await page.goto(url, { waitUntil: "networkidle0" });
+                await page.setViewport({ width: 1080, height: 1024 });
 
-            const links = await page.evaluate(() => {
-                const linkElements = document.querySelectorAll('a[backgroundsource]');
-                const hrefs = [];
-                linkElements.forEach(element => {
-                    hrefs.push(element.href);
+                // await page.waitForSelector('a[backgroundsource]', { timeout: 5000 });
+
+                const links = await page.evaluate(() => {
+                    const linkElements = document.querySelectorAll('a');
+                    const hrefs = new Set();
+                    linkElements.forEach(element => {
+                        hrefs.add(element.href);
+                    });
+                    return Array.from(hrefs);
                 });
-                return hrefs;
-            });
 
-            if (links.length > 0) {
-                offersFound = true;
-                links.forEach(link => {
-                    const parts = link.split('/companies/');
-                    if (parts.length === 2) {
-                        const companyId = parts[1];
-                        results.push(`${apiLink}${companyId}`);
-                    }
-                });
-            } else {
-                console.log("Pas d'offre sur la page", pageNumber);
+                const jobUrlPattern = /\/companies\/[^\/]+\/jobs\/[^\/]+/;
+
+                const jobOfferUrls = links.filter(url => jobUrlPattern.test(url));
+
+                if (jobOfferUrls.length > 0) {
+                    offersFound = true;
+                    jobOfferUrls.forEach(link => {
+                        const parts = link.split('/companies/');
+                        if (parts.length === 2) {
+                            const companyId = parts[1];
+                            results.push(`${apiLink}${companyId}`);
+                        }
+                    });
+                } else {
+                    // console.info("Pas d'offre sur la page", pageNumber);
+                    break;
+                }
+
+            } catch (error) {
+                console.info("Erreur sur la page", pageNumber);
+                await page.screenshot({ path: `error_page${pageNumber}.png`});
                 break;
             }
-
-        } catch (error) {
-            console.log("Page non trouvée", pageNumber);
-            break;
         }
+
+        if (!offersFound) {
+            // console.info("Aucune offre trouvée pour cet intitulé", jobTitle);
+        }
+    } catch (error) {
+        console.error("An error occured during scraping:", error);
+    } finally {
+        await browser.close();
     }
 
-    if (!offersFound) {
-        console.log("Aucune offre trouvée pour cet intitulé", jobTitle);
-    }
-
-    await browser.close();
     return results;
 }
 
 (async () => {
-    const jobTitle = process.argv[2];
-    const maxPage = parseInt(process.argv[3], 10);
+    const jobTitle = process.argv[2] || 'data';
+    const maxPage = parseInt(process.argv[3], 10) || 40;
     const results = await scrap(jobTitle, maxPage);
     console.log(JSON.stringify(results));
 })();
--- workflow.py
+++ workflow.py
@@ -1,24 +1,41 @@
 from prefect import flow, task
+import asyncio
 import subprocess
 import json
 from cleaner import Cleaner
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 import os
+import pickle
 from dotenv import load_dotenv
 
-dotenv_path = '/mongodb/.env'
+dotenv_path = './mongodb/.env'
 load_dotenv(dotenv_path=dotenv_path)
 
+# Load MongoDB credentials from environment
+def get_db_connection():
+    username = os.getenv('MONGO_INITDB_ROOT_USERNAME')
+    password = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
+    host = os.getenv('MONGO_HOST')
+    port = os.getenv('MONGO_PORT')
+
+    if not username or not password:
+        raise ValueError("MongoDB credentials are not set in environment variables.")
+
+    try:
+        client = MongoClient(f'mongodb://{username}:{password}@{host}:{port}/?authSource=admin')
+        return client.offers.offers
+    except errors.ConnectionFailure as e:
+        print(f"Could not connect to MongoDB: {e}")
+
+# Scrap offers from wttj
 @task
-def run_scraper(
-    job_title: str = "data",
-    max_pages: int = 5
-    ) -> list[str]:
+def run_scraper(job_title: str = "data", max_pages: int = 40) -> list[str]:
     """Execute the js script to scrap job offers from wttj and return api links."""
     command = ["node", "js_scripts/scrap.js", job_title, str(max_pages)]
 
     try:
         result = subprocess.run(command, capture_output=True, text=True, encoding="utf-8")
+        print(result.stdout[:10])
         output = json.loads(result.stdout)
         return output
     except subprocess.CalledProcessError as e:
@@ -26,10 +43,9 @@ def run_scraper(
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
 
+# Get info from scraped offers
 @task
-def api_request(
-    link: str
-    ) -> dict[str, str | float]:
+def api_request(link: str) -> dict[str, str | float]:
     """Get job offer info from API."""
     command = ["node", "js_scripts/api_request.js", link]
 
@@ -42,58 +58,95 @@ def api_request(
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
 
+# Clean info from offers
 @task
 def clean_data(job: dict[str, str | float]) -> dict[str, str | float]:
     """Clean the job offer."""
     cleaner = Cleaner(job)
-    cleaned_job = cleaner.clean_full()
-    return cleaned_job
+    return cleaner.clean_full()
 
+# Insert Data in MongoDB
 @task
-def insert_data(data: list[dict[str, str | float]]) -> None:
-    username = os.getenv('MONGO_INITDB_ROOT_USERNAME')
-    password = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
-    host = 'localhost'
-    port = 27017
-
-    if not username or not password:
-        raise ValueError("MongoDB credentials are not set in environment variables.")
-
+def insert_data(data: list[dict[str, str | float]], collection):
     try:
-        client = MongoClient(f'mongodb://{username}:{password}@{host}:{port}/?authSource=admin')
+        for offer in data:
+            existing_id = collection.find_one({'id': offer['id']})
+            if existing_id:
+                print(f'ID : "{offer["id"]}" already in db.')
+                modif_date_db = existing_id['date_modif']
+                modif_date_scrap = offer['date_modif']
+                if modif_date_db < modif_date_scrap:
+                    collection.replace_one({'id': offer['id']}, offer)
+                    print(f'Offer "{offer["id"]}" updated.')
+                else:
+                    print(f'No modifications have been made to offer "{offer["id"]}".')
+            else:
+                collection.insert_one(offer)
+                print(f'Offer "{offer["id"]}" added to db.')
+    except errors.PyMongoError as e:
+        print(f"An error occured: {e}")
 
-        db = client.offers
-        collection = db.offers
+def save_cache(data, filename):
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file)
 
-        result = collection.insert_many(data)
-        # print(f"Data inserted with id: {result.inserted_id}")
+def load_cache(filename):
+    if os.path.exists(filename):
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
+# Retrieve all IDs from MongoDB
+@task
+def get_all_db_ids(collection) -> set[str]:
+    cache_filename = 'db_ids_cache.pkl'
+    cached_ids = load_cache(cache_filename)
+    if cached_ids:
+        return cached_ids
 
-@flow(name="Scrape Job Offers", log_prints=True)
-def scrap_job_offers(
-    job_title: str = "data",
-    max_pages: int = 5,
-    ) -> None:
-    api_links = run_scraper(job_title, max_pages)
+    ids = collection.find({}, {'id': 1, '_id': 0})
+    id_set = set([doc['id'] for doc in ids])
+    save_cache(id_set, cache_filename)
+    return id_set
 
+# Remove offers that are not valid anymore
+@task
+def remove_old_offers(ids_to_del: set[str], collection):
+    try:
+        result = collection.delete_many({'id': {'$in': list(ids_to_del)}})
+        print(f'Deleted {result.deleted_count} offers.')
+    except errors.PyMongoError as e:
+        print(f'An error occurred: {e}')
+
+@flow(
+        name="Scrape Job Offers",
+        log_prints=True,
+        # schedule=IntervalSchedule(interval=timedelta(minutes=30))
+        )
+def scrap_job_offers(job_title: str = "data", max_pages: int = 40) -> None:
+    api_links = run_scraper(job_title, max_pages).result()
     result = []
 
     if api_links:
-        for link in api_links[:2]:
-            offer_info = api_request(link)
+        for link in api_links:
+            offer_info = api_request(link).result()
             if offer_info:
-                cleaned_offer_info = clean_data(offer_info)
+                cleaned_offer_info = clean_data(offer_info).result()
                 result.append(cleaned_offer_info)
             else:
                 print(f"Failed to fetch offer details for link: {link}")
+    api_ids = {offer['id'] for offer in result}
+    if db_ids:
+        db_ids += api_ids
+    else:
+        db_ids = get_all_db_ids(collection).result()
+        db_ids += api_ids
 
-    with open("offers.json", "w", encoding="utf-8") as file:
-        json.dump(result, file, ensure_ascii=False, indent=2)
-
-    insert_data(result)
-
+    collection = get_db_connection()
+    if collection:
+        insert_data(result, collection)
+        api_ids = {offer['id'] for offer in result}
+        ids_to_del = db_ids - api_ids
+        remove_old_offers(ids_to_del, collection)
 
 if __name__ == "__main__":
     scrap_job_offers(max_pages=1)
